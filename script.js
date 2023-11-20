fe4291ebd86a57d8cb7adc076d95d2e31091d173
Myuuki1126 <109127643+Myuuki1126@users.noreply.github.com>
2023-11-20 15:14:01 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 //アイコン
  const whiteIcon = L.icon({
-   iconUrl: 'ico.png',
-   shadowUrl: 'ico_shadow.png',
+   iconUrl: 'images/ico.png',
+   shadowUrl: 'images/ico_shadow.png',
  
  iconSize:     [40, 40], // size of the icon
  shadowSize:   [40, 40], // size of the shadow
@@ -18,4 +18,35 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
  popupAnchor:  [0, 42] // point from which the popup should open relative to the iconAnchor
  });
 
- ;L.marker([33.670683537506484, 130.4454933231803], { icon: whiteIcon }).addTo(map).bindPopup('こんにちは！');
+L.marker([33.670683537506484, 130.4454933231803], { icon: whiteIcon }).addTo(map).bindPopup('こんにちは！');
+
+const circle = L.circle([33.670683537506484, 130.4454933231803], {
+  color: 'red', //円の輪郭線の色
+  fillColor: '#f03', //円の塗りつぶしの色
+  fillOpacity: 0.3, //塗りつぶしの不透明度
+  radius: 300 //半径、メートルで指定
+}).addTo(map);
+circle.bindPopup("半径300mの範囲");
+
+// 多角形の表示
+const polygon = L.polygon([
+  [33.673461, 130.445266],
+  [33.677711, 130.44591],
+  [33.673854, 130.441146]
+], {
+  color: 'blue',
+  fillColor: '#30f',
+  fillOpacity: 0.3
+}).addTo(map);
+
+// クリック位置の緯度経度表示
+const popup = L.popup();
+
+function onMapClick(e) {
+  popup
+    .setLatLng(e.latlng)
+    .setContent("ここは" + e.latlng.toString() + "です")
+    .openOn(map);
+}
+
+map.on('click', onMapClick);
